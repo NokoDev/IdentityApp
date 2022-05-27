11ae90f4007c7d2451642aec98c244012308df0b
NokoDev <demozflava@gmail.com>
2022-05-27 14:22:04 +0200

    added the local mongo connection

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const app = express()
 const mongoose = require("mongoose")
 const port = 3000
 
+
+mongoose.connect("mongodb://localhost:27017/UserAccount")
+.then(()=>{
+    console.log("DB Connection open...")
+}).catch(err =>{
+    console.log(`Something went wrong: ${error}`)
+})
 app.get("/", (req, res)=>{
     res.send("welcome world!!")
 })
