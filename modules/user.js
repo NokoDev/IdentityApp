35cbcc78f1c799cf512e80908a68b1982fbf4aed
NokoDev <demozflava@gmail.com>
2022-05-27 14:32:33 +0200

    created a dump schema

---
+++ modules/user.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema;
+
+
+const UserSchema = new Schema({
+    
+    
+    cn: String,
+    title: String,
+    company: {
+        type: String,
+        enum: "CyberArk"
+    },
+    location: {
+        type: String,
+        enum: "Gauteng"
+    },
+
+})
